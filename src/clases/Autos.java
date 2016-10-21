056974761ef06e39210d5518cde2c3b2b8dd6b32
jparedes17 <paredesjorge10@hotmail.com>
2016-10-21 09:17:05 -0500

    Parte Inicial

---
+++ src/clases/Autos.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author pared
+ */
+public class Autos implements java.io.Serializable{
+      private String nombre;
+      private String apellido;
+      private String Cedula;
+      private String Modelo;
+      private String Marca;
+      private String precio;
+      private String tipomodelo;
+      private String tipomarca;
+
+    public Autos(String nombre, String apellido, String Cedula, String Modelo, String Marca, String tipomodelo, String tipomarca) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Cedula = Cedula;
+        this.Modelo = Modelo;
+        this.Marca = Marca;
+        this.tipomodelo = tipomodelo;
+    }
+    
+ public Autos(String Modelo, String Marca, String tipomodelo, String tipomarca, String precio) {
+        this.Modelo = Modelo;
+        this.Marca = Marca;
+        this.tipomodelo = tipomodelo;
+        this.tipomarca = tipomarca;
+        this.precio = precio;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String Modelo) {
+        this.Modelo = Modelo;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getTipomodelo() {
+        return tipomodelo;
+    }
+
+    public void setTipomodelo(String tipomodelo) {
+        this.tipomodelo = tipomodelo;
+    }
+
+    public String getTipomarca() {
+        return tipomarca;
+    }
+
+    public void setTipomarca(String tipomarca) {
+        this.tipomarca = tipomarca;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+      
+       public void guardar(ObjectOutputStream salida) throws IOException{
+        salida.writeObject(this);
+    }
+    
+    }
+    
+
---
+++ src/clases/Helper.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.awt.Component;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author pared
+ */
+public class Helper {
+    public static void mensaje(Component ventana, String mensaje, int tipo) {
+        switch (tipo) {
+            case 1:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Información", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Advertencia", JOptionPane.WARNING_MESSAGE);
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
+                break;
+
+        }
+    }
+    
+    public static void limpiadoTabla(JTable tabla1) {
+        int nf, nc;
+        nc = tabla1.getColumnCount();
+        nf = tabla1.getRowCount();
+        for (int i = 0; i < nf; i++) {
+            for (int j = 0; j < nc; j++) {
+                tabla1.setValueAt("", i, j);
+            }
+        }
+    }
+
+    public static void porDefectoTabla(JTable tabla1) {
+        DefaultTableModel tm;
+        tm = (DefaultTableModel) tabla1.getModel();
+        tm.setColumnCount(0);
+        tm.setRowCount(0);
+    }
+    
+    public static void habilitarBotones(JButton[] botones) {
+        for (int i = 0; i < botones.length; i++) {
+            botones[i].setEnabled(true);
+
+        }
+    }
+
+    public static void deshabilitarBotones(JButton[] botones) {
+        for (int i = 0; i < botones.length; i++) {
+            botones[i].setEnabled(false);
+
+        }
+    }
+    
+    public static void llenarTabla(JTable tabla, String ruta){
+        DefaultTableModel tm;
+        int nf;
+        ArrayList <Autos> auto = traerDatos(ruta);
+        tm = (DefaultTableModel)tabla.getModel();
+        limpiadoTabla(tabla);
+        nf = auto.size();
+        tm.setRowCount(nf);
+        for (int i = 0; i < nf; i++) {
+           tabla.setValueAt(i+1, i, 0);
+           tabla.setValueAt(auto.get(i).getMarca(), i, 3);
+           tabla.setValueAt(auto.get(i).getModelo(), i, 4);
+           tabla.setValueAt(auto.get(i).getTipomarca(), i, 2);
+           tabla.setValueAt(auto.get(i).getTipomodelo(), i, 1);
+        }
+    }
+    
+    public static ArrayList traerDatos(String ruta){
+        FileInputStream archivo;
+        ObjectInputStream entrada;
+        ArrayList personas = new ArrayList();
+        Object p;
+        
+        try {
+            archivo = new FileInputStream(ruta);
+            entrada = new ObjectInputStream(archivo);
+            while((p=entrada.readObject())!=null){
+                personas.add(p);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+             System.out.println(ex.getMessage());
+        }
+           return personas;
+    } 
+    
+    public static void volcado(ObjectOutputStream salida,ArrayList personas ){
+        for (int i = 0; i < personas.size(); i++) {
+            try {
+                salida.writeObject(personas.get(i));
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            
+        }
+    }
+}
